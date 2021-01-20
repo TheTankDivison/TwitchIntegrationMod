1efade03a385d1dfa242ec09cb751fd80a99e413
TheTankDivison <jdzlat@mail.ru>
2021-01-20 20:36:05 +0500

    v

--- TwitchIntegrationMod/Hooks/packages.lua
+++
@@ -1,6 +0,0 @@
-if Global.load_level then
-	if not PackageManager:loaded("levels/narratives/e_election_day/stage_3/world/world") then
-		PackageManager:load("levels/narratives/e_election_day/stage_3/world/world")
-	end
-end
-
