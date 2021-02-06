cbc8696d3cc45c0dc88a49e2c1f6acdc1dc98f3d
TheTankDivison <jdzlat@mail.ru>
2021-02-06 22:15:07 +0500

    Update medic.lua

--- TwitchIntegrationMod/Effects/medic/medic.lua
+++ TwitchIntegrationMod/Effects/medic/medic.lua
@@ -7,16 +7,16 @@ options_end
 function TIM.effectsFunctions.medic(rewardID)
 	managers.player:local_player():sound():say("Play_ban_s05",true,true)
 	local pos
-	managers.mission._fading_debug_output:script().log(tostring(TIM._settings.TwitchRewards[rewardID].effects.medic.Count),  Color.green)
+	--managers.mission._fading_debug_output:script().log(tostring(TIM._settings.TwitchRewards[rewardID].effects.medic.Count),  Color.green)
 	for i=1, TIM._settings.TwitchRewards[rewardID].effects.medic.Count, 1 do
-	managers.mission._fading_debug_output:script().log(tostring(2),  Color.green)
+	--managers.mission._fading_debug_output:script().log(tostring(2),  Color.green)
 		local unit_name = Idstring("units/payday2/characters/ene_medic_r870/ene_medic_r870")
 		local unit_done
-		managers.mission._fading_debug_output:script().log(tostring(3),  Color.green)
+		--managers.mission._fading_debug_output:script().log(tostring(3),  Color.green)
 		pos, unit_done = TIM:Spawn_unit(unit_name, true)
-		managers.mission._fading_debug_output:script().log(tostring(4),  Color.green)
+		--managers.mission._fading_debug_output:script().log(tostring(4),  Color.green)
 		local lin = TIM:fon_function()
-		managers.mission._fading_debug_output:script().log(tostring(5),  Color.green)
+		--managers.mission._fading_debug_output:script().log(tostring(5),  Color.green)
 		lin:animate(function(o)
 			unit_done:character_damage():set_invulnerable(true)
 			unit_done:character_damage():set_immortal(true)
